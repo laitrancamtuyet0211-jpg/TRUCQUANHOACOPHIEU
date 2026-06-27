fa1715d9ae9514208becfc656b0a69c096b496e5
laitrancamtuyet0211-jpg <laitrancamtuyet0211@gmail.com>
2026-06-27 10:15:24 +0700

    Update app.py

--- app.py
+++ app.py
@@ -14,9 +14,8 @@ st.set_page_config(
     page_icon="📈",
     layout="wide"
 )
-
-st.title("📈 PHÂN TÍCH GIÁ CỔ PHIẾU VÀ KIỂM ĐỊNH MANN-KENDALL")
-
+st.image("logo.jpg")
+st.title("📈 TRỰC QUAN HOÁ GIÁ CỔ PHIẾU VÀ KIỂM ĐỊNH MANN-KENDALL_DỀ TÀI 5_LAI TRẦN CẨM TUYẾT")
 # =============================
 # SIDEBAR
 # =============================
